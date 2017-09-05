f29b8181e56cccf3a53d98cfffdd1ed52f8079b5
Quentin Civiale <quentin.civiale@gmail.com>
2017-09-05 19:17:01 +0200

    Modification de fonction

--- Controleur/controleurSignal.php
+++ Controleur/controleurSignal.php
@@ -17,18 +17,25 @@ class ControleurSignal {
   }
 
   // Affiche les commentaires
-  public function commentaire() {
+  /*public function commentaire() {
     $commentaires = $this->commentaire->getComm();
     $vue = new Vue("AdminComm");
     $vue->generer(array('commentaires' => $commentaires));
+  }*/
+    
+    // Affiche les détails sur un épisode
+  public function episodeDetail($idEpisode) {
+    $episode = $this->episode->getEpisode($idEpisode);
+    $commentaires = $this->commentaire->getCommentaires($idEpisode);
+    $vue = new Vue("EpisodeDetail");
+    $vue->generer(array('episode' => $episode, 'commentaires' => $commentaires));
   }
     
   // Signale un commentaire
-  public function signal($idCommentaire) {
+  public function signal($idCommentaire, $idEpisode) {
     // Sauvegarde du signalement du commentaire
-    $this->commentaire->signal($idCommentaire); 
+    $this->commentaire->signal($idCommentaire, $idEpisode); 
     // Actualisation de l'affichage du commentaire
-    $this->commentaire();
+    $this->episodeDetail($idEpisode);
   }
-
 }
