4fab9d217c385699811d42b670ba514521330502
Quentin Civiale <quentin.civiale@gmail.com>
2017-09-05 19:14:49 +0200

    Mise à jour de la connexion BDD en local

--- Modele/modele.php
+++ Modele/modele.php
@@ -21,7 +21,7 @@ abstract class Modele {
   private function getBdd() {
     if ($this->bdd == null) {
       // Création de la connexion
-      $this->bdd = new PDO('mysql:host=localhost;dbname=P3;charset=utf8',
+    $this->bdd = new PDO('mysql:host=localhost;dbname=P3;charset=utf8',
         'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     return $this->bdd;
