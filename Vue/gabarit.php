57b30ddae80256cf20e2379271b29cf2f59614d0
Quentin Civiale <quentin.civiale@gmail.com>
2017-09-05 19:09:30 +0200

    Modification des vues pour la mise en place de Bootstrap

--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -3,102 +3,106 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="Contenu/style.css" />
+    <link href="Contenu/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="Contenu/favicon.ico" />
     <title>
         <?= $titre ?>
     </title>
 </head>
 
+
 <body>
-    <div class="global">
-        <header>
+    <header class="navigationbar">
 
-            <!--<p>Le nouveau roman de Jean Forteroche sous forme de blog !</p>-->
+        <!--<p>Le nouveau roman de Jean Forteroche sous forme de blog !</p>-->
 
-            <?php if(!($_SESSION['Auth'])){ ?>
+        <?php if(!($_SESSION['Auth'])){ ?>
 
-            <a href="index.php">
-                <h1 id="titreBlog">Billet simple pour l'Alaska <small class="signature">par Jean Forteroche</small></h1>
-            </a>
+        <a href="index.php">
+            <h1 id="titreBlog">Billet simple pour l'Alaska <small class="signature">par Jean Forteroche</small></h1>
+        </a>
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <nav>
-                        <ul class="nav nav-pills nav-justified">
+        <div class="row">
+            <div class="col-lg-offset-1 col-lg-8">
+                <nav>
+                    <ul class="nav nav-pills nav-justified">
 
-                            <li role="nav">
-                                <a href="index.php" role="button">
-                                    <span class="glyphicon glyphicon-home" aria-hiden="true"></span> Accueil</a>
-                            </li>
+                        <li role="nav">
+                            <a href="index.php" role="button">
+                                <span class="glyphicon glyphicon-home" aria-hiden="true"></span> Accueil</a>
+                        </li>
 
-                            <li role="nav">
-                                <a href="<?= " index.php?action=Episodes " ?>">
-                                    <span class="glyphicon glyphicon-list" aria-hiden="true"></span> Episodes</a>
-                            </li>
+                        <li role="nav">
+                            <a href="<?= " index.php?action=Episodes " ?>">
+                                <span class="glyphicon glyphicon-list" aria-hiden="true"></span> Episodes</a>
+                        </li>
 
-                            <li role="nav">
-                                <a href="<?= " index.php?action=contact " ?>" role="button">
-                                    <span class="glyphicon glyphicon-envelope" aria-hiden="true"></span> Contact</a>
-                            </li>
+                        <li role="nav">
+                            <a href="<?= " index.php?action=contact " ?>" role="button">
+                                <span class="glyphicon glyphicon-envelope" aria-hiden="true"></span> Contact</a>
+                        </li>
 
-                        </ul>
-                    </nav>
-                </div>
+                    </ul>
+                </nav>
             </div>
-
-            <div class="col-lg-offset-2 col-lg-2">
-                <a href="index.php?action=formulaire"><button type="button" class="btn btn-danger" title="Connexion"><span class="glyphicon glyphicon-log-in"></span> Connexion</button></a>
+            <div class="col-lg-3">
+                <a href="index.php?action=formulaire"><button type="button" class="btn btn-success" title="Connexion"><span class="glyphicon glyphicon-log-in"></span> Connexion</button></a>
             </div>
+        </div>
 
 
 
-            <?php }
+
+
+        <?php }
 
 
                     else { ?>
 
-            <h1 id="titreBlog">Billet simple pour l'Alaska <small class="signature">par Jean Forteroche</small></h1>
+        <h1 id="titreBlog">Billet simple pour l'Alaska <small class="signature">par Jean Forteroche</small></h1>
 
-            <p>Administration du site</p>
-
-            <hr/>
-
-            <div class="row">
-                <div class="col-lg-offset-4 col-lg-4">
-                    <nav>
-                        <ul class="nav nav-pills nav-justified">
-                            <li role="nav">
-                                <a href="<?= " index.php?action=adminEp " ?>" role="button">
-                                    <span class="glyphicon glyphicon-list" aria-hiden="true"></span> Episodes</a>
-                            </li>
-                            <li role="nav">
-                                <a href="<?= " index.php?action=adminComm " ?>" role="button">
-                                    <span class="glyphicon glyphicon-comment" aria-hiden="true"></span> Commentaires</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-
-                <div class="col-lg-offset-2 col-lg-2">
-                    <a href="index.php?action=deconnexion" onclick="return(confirm('Êtes-vous sûr de vouloir vous déconnecter ?'))">
-                        <button type="button" class="btn btn-danger" title="Deconnexion"><span class="glyphicon glyphicon-log-out"></span> Deconnexion</button>
-                    </a>
-                </div>
-            </div>
-            <?php } ?>
-
-        </header>
-
-        <br/>
-
-        <div id="contenu">
-            <?= $contenu ?>
+        <div class="admin">
+            <h2>Administration du site</h2>
         </div>
-        <!-- #contenu -->
 
+        <div class="row">
+            <div class="col-lg-offset-4 col-lg-4">
+                <nav>
+                    <ul class="nav nav-pills nav-justified">
+                        <li role="nav">
+                            <a href="<?= " index.php?action=adminEp " ?>" role="button">
+                                <span class="glyphicon glyphicon-list" aria-hiden="true"></span> Episodes</a>
+                        </li>
+                        <li role="nav">
+                            <a href="<?= " index.php?action=adminComm " ?>" role="button">
+                                <span class="glyphicon glyphicon-comment" aria-hiden="true"></span> Commentaires</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="col-lg-offset-2 col-lg-2">
+                <a href="index.php?action=deconnexion" onclick="return(confirm('Êtes-vous sûr de vouloir vous déconnecter ?'))">
+                    <button type="button" class="btn btn-danger" title="Deconnexion"><span class="glyphicon glyphicon-log-out"></span> Deconnexion</button>
+                </a>
+            </div>
+        </div>
+        <?php } ?>
+
+    </header>
+
+    <div class="row">
+        <div class="bannerImage">
+        </div>
     </div>
-    <!-- #global -->
+
+    <div id="contenu">
+        <?= $contenu ?>
+    </div>
+
 
     <footer>
         <div>
@@ -115,6 +119,12 @@
             </div>
         </div>
     </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="Contenu/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
 
 
 </body>
